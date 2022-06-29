01854906062dda559095923aa7bb90abfbdecb22
Kyle Roth <kylrth@gmail.com>
2022-06-29 13:52:28 -0600

    record results to CSV instead of pickles
    This is a lot more performant on a distributed filesystem. Also shortens
    the experiment ID.

--- utils.py
+++ utils.py
@@ -25,7 +25,22 @@ class TrainConfig:
     grad_accum_factor: int = 1
 
     def exp_id(self):
-        return repr(self)[12:-1]  # remove "TrainConfig(...)" wrap
+        out = (
+            f"{self.dataset},{self.model_name},pretrained={self.pretrained},"
+            f"batch_size={self.batch_size},epochs={self.epochs},objective={self.objective}"
+        )
+
+        if self.objective == "irm":
+            out += f",irm_weight={self.irm_weight},irm_anneal={self.irm_anneal}"
+        elif self.objective == "ilc":
+            out += f",ilc_agreement_threshold={self.ilc_agreement_threshold}"
+
+        out += (
+            f",lr={self.lr},momentum={self.momentum},lr_step={self.lr_step}"
+            f",weight_decay={self.weight_decay},seed={self.seed}"
+        )
+
+        return out
 
     def dataset_groups(self):
         if self.dataset == "celebA":
@@ -61,16 +76,16 @@ class Recorder:
         tb_base: str = "./runs",
         ckpt_base: str = "./checkpoints",
     ):
-        tb_base = os.path.expanduser(tb_base)
-        ckpt_base = os.path.expanduser(ckpt_base)
-
-        exp_id = config.exp_id()
+        self.exp_id = config.exp_id()
+        self.tb_dir = os.path.join(os.path.expanduser(tb_base), self.exp_id)
+        self.ckpt_dir = os.path.join(os.path.expanduser(ckpt_base), self.exp_id)
 
         # count the number of groups and the valset group counts
         self.n_groups = grouper.cardinality.prod().item()
         self.valcounts = count_groups(valset, grouper)
         self.grouper = grouper
 
+        self.config = config
         self.hparams = {
             "dataset": config.dataset,
             "model_name": config.model_name,
@@ -94,55 +109,81 @@ class Recorder:
             self.hparams["ilc_agreement_threshold"] = config.ilc_agreement_threshold
 
         os.makedirs(tb_base, exist_ok=True)
-        self.board = SummaryWriter(os.path.join(tb_base, exp_id))
-
-        self.ckpt_dir = os.path.join(ckpt_base, exp_id)
+        self.board = SummaryWriter(self.tb_dir)
         os.makedirs(self.ckpt_dir, exist_ok=True)
 
-    def report_train(self, it, agg_loss, agg_penalty, losses, penalties, counts):
+        # this is the header we'll use for all the output CSV files
+        self.train_csv_header = ["iter", "avg", "penalty"]
+        self.valid_csv_header = ["epoch", "avg"]
+        for idx in range(self.n_groups):
+            self.train_csv_header.append(self.grouper.group_str(idx))
+            self.valid_csv_header.append(self.grouper.group_str(idx))
+
+        # we'll append to the files in case we're starting from a checkpoint
+        self.train_loss_f = open(os.path.join(self.tb_dir, "train_loss.csv"), "a+")
+        self.val_loss_f = open(os.path.join(self.tb_dir, "val_loss.csv"), "a+")
+        self.val_acc_f = open(os.path.join(self.tb_dir, "val_acc.csv"), "a+")
+
+        # write the headers
+        train_header = ",".join(f'"{k}"' if "," in k else k for k in self.train_csv_header) + "\n"
+        valid_header = ",".join(f'"{k}"' if "," in k else k for k in self.valid_csv_header) + "\n"
+        self.train_loss_f.write(train_header)
+        self.val_loss_f.write(valid_header)
+        self.val_acc_f.write(valid_header)
+
+    def _dict_to_csv(self, d, is_valid: bool) -> str:
+        if is_valid:
+            header = self.valid_csv_header
+        else:
+            header = self.train_csv_header
+        return (
+            ",".join(
+                str(d[k].item()) if isinstance(d[k], torch.Tensor) else str(d[k]) for k in header
+            )
+            + "\n"
+        )
+
+    def report_train(self, it, agg_loss, group_losses, counts, agg_penalty):
         """Report train losses across the groups."""
-        self.board.add_scalars(
-            "train loss",
-            {
-                self.grouper.group_str(idx): losses[idx] / counts[idx]
-                for idx in range(self.n_groups)
-                if counts[idx] > 0
-            },
-            it,
-        )
-        self.board.add_scalars("train loss", {"avg": agg_loss}, it)
+        loss_dict = {}
+        for idx in range(self.n_groups):
+            if counts[idx] <= 0:
+                loss_dict[self.grouper.group_str(idx)] = -1.0
+                continue
+            loss_dict[self.grouper.group_str(idx)] = group_losses[idx] / counts[idx]
+        loss_dict["avg"] = agg_loss / self.config.batch_size
+        # Unlike the loss, the penalty is already aggregated so we take the average over gradient
+        # accumulation runs.
+        loss_dict["penalty"] = agg_penalty / self.config.grad_accum_factor
 
-        self.board.add_scalars(
-            "train penalty",
-            {
-                self.grouper.group_str(idx): penalties[idx] / counts[idx]
-                for idx in range(self.n_groups)
-                if counts[idx] > 0
-            },
-            it,
-        )
-        self.board.add_scalars("train penalty", {"avg": agg_penalty}, it)
-
-    def close(self):
-        self.board.close()
+        self.board.add_scalars("train loss", loss_dict, it)
+        loss_dict["iter"] = it
+        self.train_loss_f.write(self._dict_to_csv(loss_dict, False))
 
     def report_valid(self, epoch, losses, accs):
         """Report validation losses and accuracies across the groups."""
-        val_losses = {
-            self.grouper.group_str(idx): losses[idx] / self.valcounts[idx]
-            for idx in range(self.n_groups)
-            if self.valcounts[idx] > 0
-        }
+        val_losses = {}
+        for idx in range(self.n_groups):
+            if self.valcounts[idx] <= 0:
+                val_losses[self.grouper.group_str(idx)] = -1.0
+                continue
+            val_losses[self.grouper.group_str(idx)] = losses[idx] / self.valcounts[idx]
         val_losses["avg"] = sum(losses) / sum(self.valcounts)
-        self.board.add_scalars("val loss", val_losses, epoch)
 
-        val_accs = {
-            self.grouper.group_str(idx): accs[idx] / self.valcounts[idx]
-            for idx in range(self.n_groups)
-            if self.valcounts[idx] > 0
-        }
+        val_accs = {}
+        for idx in range(self.n_groups):
+            if self.valcounts[idx] <= 0:
+                val_accs[self.grouper.group_str(idx)] = -1.0
+                continue
+            val_accs[self.grouper.group_str(idx)] = accs[idx] / self.valcounts[idx]
         val_accs["avg"] = sum(accs) / sum(self.valcounts)
+
+        self.board.add_scalars("val loss", val_losses, epoch)
         self.board.add_scalars("val acc", val_accs, epoch)
+        val_losses["epoch"] = epoch
+        val_accs["epoch"] = epoch
+        self.val_loss_f.write(self._dict_to_csv(val_losses, True))
+        self.val_acc_f.write(self._dict_to_csv(val_accs, True))
 
         self.hparams["epochs"] = epoch
         metrics = {
@@ -156,6 +197,12 @@ class Recorder:
         self.board.add_hparams(self.hparams, metrics, self.discrete_hparams)
 
         self.board.flush()
+
+    def close(self):
+        self.board.close()
+        self.train_loss_f.close()
+        self.val_loss_f.close()
+        self.val_acc_f.close()
 
     def checkpoint(self, epoch, model, optim, sched):
         """Store a checkpoint for a completed epoch."""
